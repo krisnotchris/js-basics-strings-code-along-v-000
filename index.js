8d3742510cff3eecc7efde6d115e0502e5710d58
Kris Henderson <krishenderson@gmail.com>
2018-10-13 22:18:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 let currentUser = 'Kris';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ';
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}`;
